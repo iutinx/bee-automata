3d2e900088de9dc5deeecbea5a8db2a0cb208473
iuteen <iuteendev@outlook.com>
2026-08-20 03:33:22 +0300

    refactor: switch from Carbon API to NSEvent monitors with calibrate hotkey

--- BeeAutomata/Hotkeys/GlobalHotkeyManager.swift
+++ BeeAutomata/Hotkeys/GlobalHotkeyManager.swift
@@ -1,57 +1,75 @@
-import Carbon
 import AppKit
 
 final class GlobalHotkeyManager {
 
-    private var hotkeyRef: EventHotKeyRef?
+    private var toggleGlobalMonitor: Any?
+    private var calibrateGlobalMonitor: Any?
+    private var toggleLocalMonitor: Any?
+    private var calibrateLocalMonitor: Any?
     private var onToggle: (() -> Void)?
+    private var onCalibrate: (() -> Void)?
 
     private static var instance: GlobalHotkeyManager?
 
-    static func register(toggleHandler: @escaping () -> Void) -> GlobalHotkeyManager {
+    private static let toggleKeyCode: UInt16 = 46
+    private static let calibrateKeyCode: UInt16 = 8
+    private static let requiredModifiers: NSEvent.ModifierFlags = [.command, .shift]
+
+    static func register(
+        toggleHandler: @escaping () -> Void,
+        calibrateHandler: @escaping () -> Void
+    ) -> GlobalHotkeyManager {
         let manager = GlobalHotkeyManager()
         manager.onToggle = toggleHandler
-        manager.installHandler()
+        manager.onCalibrate = calibrateHandler
+        manager.installMonitors()
         instance = manager
         return manager
     }
 
-    private func installHandler() {
-        var eventType = EventTypeSpec(
-            eventClass: OSType(kEventClassKeyboard),
-            eventKind: UInt32(kEventHotKeyPressed)
-        )
+    private func installMonitors() {
+        toggleGlobalMonitor = NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { [weak self] event in
+            self?.handleKeyEvent(event)
+        }
 
-        let selfPtr = Unmanaged.passUnretained(self).toOpaque()
+        calibrateGlobalMonitor = NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { [weak self] event in
+            self?.handleKeyEvent(event)
+        }
 
-        InstallEventHandler(
-            GetApplicationEventTarget(),
-            { (_, event, userData) -> OSStatus in
-                guard let userData = userData else { return noErr }
-                let manager = Unmanaged<GlobalHotkeyManager>.fromOpaque(userData).takeUnretainedValue()
-                manager.onToggle?()
-                return noErr
-            },
-            1,
-            &eventType,
-            selfPtr,
-            nil
-        )
+        toggleLocalMonitor = NSEvent.addLocalMonitorForEvents(matching: .keyDown) { [weak self] event in
+            self?.handleKeyEvent(event)
+            return event
+        }
 
-        let hotkeyID = EventHotKeyID(signature: 0x42534153, id: 1)
-        RegisterEventHotKey(
-            UInt32(kVK_F6),
-            UInt32(0),
-            hotkeyID,
-            GetApplicationEventTarget(),
-            0,
-            &hotkeyRef
-        )
+        calibrateLocalMonitor = NSEvent.addLocalMonitorForEvents(matching: .keyDown) { [weak self] event in
+            self?.handleKeyEvent(event)
+            return event
+        }
+    }
+
+    private func handleKeyEvent(_ event: NSEvent) {
+        let modifiers = event.modifierFlags.intersection(.deviceIndependentFlagsMask)
+        guard modifiers == Self.requiredModifiers else { return }
+
+        if event.keyCode == Self.toggleKeyCode {
+            onToggle?()
+        } else if event.keyCode == Self.calibrateKeyCode {
+            onCalibrate?()
+        }
     }
 
     deinit {
-        if let ref = hotkeyRef {
-            UnregisterEventHotKey(ref)
+        if let monitor = toggleGlobalMonitor {
+            NSEvent.removeMonitor(monitor)
+        }
+        if let monitor = calibrateGlobalMonitor {
+            NSEvent.removeMonitor(monitor)
+        }
+        if let monitor = toggleLocalMonitor {
+            NSEvent.removeMonitor(monitor)
+        }
+        if let monitor = calibrateLocalMonitor {
+            NSEvent.removeMonitor(monitor)
         }
     }
 }
